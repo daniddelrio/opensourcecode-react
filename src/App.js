73ddf25f06db56d85fe89a647aa0aa3c0ccd0125
Daniel <daniel.delrio@obf.ateneo.edu>
2020-10-04 18:47:04 +0800

    Set up React and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import history from './history'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/register" exact component={Register} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/verify_email" exact component={VerifyEmail} />
+          <Route path="/activate/:uidb64/:token" exact component={VerifyToken} />
+          <Route path="/details" exact component={Details} />
+        </Switch>
+    </Router>
   );
 }
-
-export default App;
