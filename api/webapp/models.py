a28e2e9e2017bb3e10781da1233ae8f7545786a9
Daniel <daniel.delrio@obf.ateneo.edu>
2020-10-04 23:10:17 +0800

    Set up Django and models

---
+++ api/webapp/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=255)
+    teacher_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Class(models.Model):
+    number = models.IntegerField(validators=[
+        MaxValueValidator(12),
+        MinValueValidator(1)
+    ])
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.number
+
+class Section(models.Model):
+    SECTION_CHOICES = [
+        ('A', 'A'),
+        ('B', 'B'),
+        ('C', 'C'),
+        ('D', 'D'),
+        ('E', 'E'),
+        ('F', 'F'),
+    ]
+
+    name = models.CharField(max_length=5, choices=SECTION_CHOICES)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.number
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    roll_number = models.IntegerField()
+    class_num = models.ForeignKey(Class, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
